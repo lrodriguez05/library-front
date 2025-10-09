0efaec3f41436655af2e45fc487d4669f63198d7
Lazaro <lazarorodriguezleyva@gmail.com>
2025-10-09 01:25:08 -0400

    Add delete functionality for books in Libro component

--- src/components/home_components/libro.jsx
+++ src/components/home_components/libro.jsx
@@ -1,6 +1,7 @@
 import { Table } from "antd";
-import { useEffect, useState } from "react";
+import { useEffect, useState, createContext, useContext } from "react";
 import { Link } from "react-router";
+import { Trash } from "lucide-react";
 
 const columns = [
   {
@@ -28,7 +29,44 @@ const columns = [
     dataIndex: "sede",
     key: "sede",
   },
+  {
+    title: "Action",
+    dataIndex: "",
+    key: "action",
+    render: (data) => {
+      return <Delete bookId={data.id} key={data.id} />;
+    },
+  },
 ];
+
+function Delete({ bookId }) {
+  const { fetchLibros, data } = useContext(BookContext);
+
+  const handleDeleteBook = () => {
+    fetch(`${import.meta.env.VITE_API_URL}/lib/libros/${bookId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        fetchLibros();
+      })
+      .catch((error) => {
+        console.error("Error al eliminar el libro:", error);
+      });
+  };
+
+  return (
+    <div onClick={handleDeleteBook}>
+      <Trash size={20} />
+    </div>
+  );
+}
+
+const BookContext = createContext();
 
 function Libro() {
   const [data, setData] = useState([]);
@@ -68,22 +106,24 @@ function Libro() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-2">
-      <h2>Hola desde libros</h2>
-      <button
-        onClick={fetchLibros}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Refrescar libros
-      </button>
-      <Link
-        to={"/crearLibro"}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
-      >
-        Añadir Libro
-      </Link>
-      <Table dataSource={data} columns={columns} loading={loading}></Table>
-    </div>
+    <BookContext.Provider value={{ fetchLibros, data }}>
+      <div className="flex flex-col gap-2">
+        <h2>Hola desde libros</h2>
+        <button
+          onClick={fetchLibros}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Refrescar libros
+        </button>
+        <Link
+          to={"/crearLibro"}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
+        >
+          Añadir Libro
+        </Link>
+        <Table dataSource={data} columns={columns} loading={loading}></Table>
+      </div>
+    </BookContext.Provider>
   );
 }
 
